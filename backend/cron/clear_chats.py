6df084c4765f7fd0fc228ef92d52c4728e7baf08
reviewgramweb <reviewgramgithub@mail.ru>
2020-03-01 22:04:19 +0400

    [BACKEND] Регистрация связи токен <-> чат и очистка её.

--- backend/cron/clear_chats.py
+++ backend/cron/clear_chats.py
@@ -1,5 +1,5 @@
 # encoding=UTF-8
-# Очищает истекшие токены, удаляя их ищ БД
+# Очищает истекшие ссылки на чаты, удаляя их ищ БД
 from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 import json
@@ -8,19 +8,14 @@ import pymysql
 import traceback
 import time
 
-
 path = os.path.dirname(os.path.abspath(__file__))
 env_path = Path(path + "/../") / '.env'
 load_dotenv(dotenv_path=env_path)
 timestamp = int(time.time())
-cleanupTime = int(os.getenv("TOKEN_CLEANUP_TIME")) * 60
+cleanupTime = int(os.getenv("CHAT_CACHE_TOKEN_SECONDS"))
 con = pymysql.connect(os.getenv("MYSQL_HOST"), os.getenv("MYSQL_USER"), os.getenv("MYSQL_PASSWORD"), os.getenv("MYSQL_DB"))
 with con:
     cur = con.cursor()
-    cur.execute("DELETE FROM `token_to_chat_id` WHERE `TOKEN` IN (SELECT `TOKEN` FROM `token_to_user_id` WHERE " + str(timestamp) + " - TSTAMP >= " + str(cleanupTime) + ")")
-    con.commit()
-    cur.close()
-    cur = con.cursor()
-    cur.execute("DELETE FROM `token_to_user_id` WHERE " + str(timestamp) + " - TSTAMP >= " + str(cleanupTime))
+    cur.execute("DELETE FROM `token_to_chat_id` WHERE  " + str(timestamp) + " - TSTAMP >= " + str(cleanupTime) + "")
     con.commit()
     cur.close()
--- backend/reviewgram.py
+++ backend/reviewgram.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, jsonify, abort
 from dotenv import load_dotenv, find_dotenv
+from datetime import datetime
 import logging
 import json
 import os
 import pymysql
 import base64
 import traceback
+import requests
+import time
 
 load_dotenv(find_dotenv())
 
@@ -14,6 +17,7 @@ bot_api_token  = os.getenv("BOT_API_TOKEN")
 
 app = Flask(__name__)
 
+# Получение вложенных данных из словаря
 def safe_get_key(dict, keys):
     tmp = dict
     for key in keys:
@@ -23,15 +27,62 @@ def safe_get_key(dict, keys):
             return None
     return tmp
 
+# Соединение с БД
+def connect_to_db():
+    return pymysql.connect(os.getenv("MYSQL_HOST"), os.getenv("MYSQL_USER"), os.getenv("MYSQL_PASSWORD"), os.getenv("MYSQL_DB"))
+
+# Получение первой строки по запросу из БД
+def select_and_fetch_one(con, query, params):
+    cur = con.cursor()
+    cur.execute(query, params)
+    result = cur.fetchone()
+    cur.close()
+    return result
+
+# Получение первой колонки и строки по запросу из БД
+def select_and_fetch_first_column(con, query, params):
+    row  = select_and_fetch_one(con, query, params)
+    if (row is None):
+        return None
+    else:
+        return row[0]
+
+# Выполнение запроса к БД
+def execute_update(con, query, params):
+    cur = con.cursor()
+    cur.execute(query, params)
+    con.commit()
+    cur.close()
+
+# Запись данных в лог
+def append_to_log(text):
+    date_time_now = datetime.now()
+    str_date_time = date_time_now.strftime("%d-%m-%Y (%H:%M:%S)")
+    text_file = open(os.getenv("APP_FOLDER") + "/log.txt", "a")
+    text_file.write("["  + str_date_time + "]" + text + "\n")
+    text_file.close()
+
+# Вставляет или обновляет соотношение токена с чатом
+def  insert_or_update_token_to_chat(con, chatId, uuid):
+    query = "SELECT COUNT(*) AS CNT FROM `token_to_chat_id` WHERE `TOKEN` = %s AND `CHAT_ID` = %s"
+    countRows = select_and_fetch_first_column(con, query, [uuid, chatId])
+    if (countRows > 0):
+        execute_update(con, "UPDATE `token_to_chat_id` SET TSTAMP = UNIX_TIMESTAMP(NOW()) WHERE TOKEN = %s AND CHAT_ID = %s", [uuid, chatId])
+    else:
+        execute_update(con, "INSERT INTO `token_to_chat_id`(TOKEN, CHAT_ID, TSTAMP) VALUES (%s, %s, UNIX_TIMESTAMP(NOW()))", [uuid, chatId])
+
 @app.route('/')
 def index():
-    return 'Hello world'
+    return 'OK'
 
 
 @app.route('/reviewgram/')
 def reviewgram():
-    return 'Hello world'
+    return 'OK'
 
+@app.route('/reviewgram/bot_username/')
+def bot_username():
+    return os.getenv("BOT_USERNAME")
 
 @app.route('/reviewgram/bot/', methods=['POST', 'GET'])
 def bot_api():
@@ -45,41 +96,66 @@ def bot_api():
         message = safe_get_key(data, ["message", "text"])
         if ((userId is not None) and (message is not None)):
             decoded = base64.b64decode(message)
-            con = pymysql.connect(os.getenv("MYSQL_HOST"), os.getenv("MYSQL_USER"), os.getenv("MYSQL_PASSWORD"), os.getenv("MYSQL_DB"))
+            con = connect_to_db()
             with con:
-                cur = con.cursor()
-                cur.execute("SELECT COUNT(*) AS CNT FROM `token_to_user_id` WHERE `TOKEN` = %s", [decoded])
-                countRows = cur.fetchone()
-                cur.close()
-                if (countRows[0] > 0):
-                    cur = con.cursor()
-                    cur.execute("UPDATE `token_to_user_id` SET USER_ID = %s, TSTAMP = UNIX_TIMESTAMP(NOW()) WHERE TOKEN = %s", [userId, decoded])
-                    con.commit()
-                    cur.close()
+                countRows = select_and_fetch_first_column(con, "SELECT COUNT(*) AS CNT FROM `token_to_user_id` WHERE `TOKEN` = %s", [decoded])
+                if (countRows > 0):
+                    execute_update(con, "UPDATE `token_to_user_id` SET USER_ID = %s, TSTAMP = UNIX_TIMESTAMP(NOW()) WHERE TOKEN = %s", [userId, decoded])
                 else:
-                    cur = con.cursor()
-                    cur.execute("INSERT INTO `token_to_user_id`(USER_ID, TOKEN, TSTAMP) VALUES (%s, %s, UNIX_TIMESTAMP(NOW()))", [userId, decoded])
-                    con.commit()
-                    cur.close()
+                    execute_update(con, "INSERT INTO `token_to_user_id`(USER_ID, TOKEN, TSTAMP) VALUES (%s, %s, UNIX_TIMESTAMP(NOW()))", [userId, decoded])
         else:
-            text_file = open("/root/reviewgram/log.txt", "a")
-            text_file.write("no data")
-            text_file.write("\n")
-            text_file.close()
+            append_to_log("/reviewgram/bot: no data")
             abort(404)
     except Exception as e:
-        text_file = open("/root/reviewgram/log.txt", "a")
-        text_file.write("Exception " + traceback.format_exc())
-        text_file.write("\n")
-        text_file.close()
+        append_to_log("/reviewgram/bot: Exception " + traceback.format_exc())
         abort(404)
     return 'OK'
 
 
-@app.route('/reviewgram/bot_username/')
-def bot_username():
-    return os.getenv("BOT_USERNAME")
-
+@app.route('/reviewgram/register_chat_id_for_token/', methods=['POST'])
+def register_chat_id_for_token():
+    chatId = request.values.get("chatId")
+    uuid = request.values.get("uuid")
+    try:
+        if ((chatId is not None) and (uuid is not None)):
+            chatId = int(chatId)
+            con = connect_to_db()
+            with con:
+                timestamp = int(time.time())
+                tokenCleanupTime = int(os.getenv("TOKEN_CLEANUP_TIME")) * 60
+                chatCleanupTime = int(os.getenv("CHAT_CACHE_TOKEN_SECONDS"))
+                query = "SELECT USER_ID FROM `token_to_user_id` WHERE `TOKEN` = %s AND " + str(timestamp) + " - TSTAMP <= " + str(tokenCleanupTime) + " LIMIT 1"
+                userId = select_and_fetch_first_column(con, query, [uuid])
+                if (userId is not None):
+                    query = "SELECT ID FROM `token_to_chat_id` WHERE `TOKEN` = %s AND `CHAT_ID` = %s AND " + str(timestamp) + " - TSTAMP <= " + str(chatCleanupTime) + " LIMIT 1"
+                    row = select_and_fetch_first_column(con, query, [uuid, chatId])
+                    if (row is None):
+                        url = "https://api.telegram.org/bot" + os.getenv("BOT_API_TOKEN") + "/getChatMember"
+                        append_to_log("/reviewgram/register_chat_id_for_token/: " + url)
+                        params = {'user_id': userId,  'chat_id' : chatId}
+                        response = requests.get(url, params=params)
+                        json_response = response.json()
+                        if (json_response is None):
+                            append_to_log("/reviewgram/register_chat_id_for_token/: unable to parse response")
+                            abort(404)
+                        else:
+                            if (json_response["ok"] is True):
+                                insert_or_update_token_to_chat(con, chatId, uuid)
+                            else:
+                                append_to_log("/reviewgram/register_chat_id_for_token/: TG API reported that user is not in chat")
+                                abort(404)
+                    else:
+                        return 'OK'
+                else:
+                    append_to_log("/reviewgram/register_chat_id_for_token/: user token not found")
+                    abort(404)
+                return 'OK'
+        else:
+            append_to_log("/reviewgram/register_chat_id_for_token/: no data")
+            abort(404)
+    except Exception as e:
+        append_to_log("/reviewgram/register_chat_id_for_token/: Exception " + traceback.format_exc())
+        abort(404)
 
 gunicorn_logger = logging.getLogger("gunicorn.error")
 app.logger.handlers = gunicorn_logger.handlers
